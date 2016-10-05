39f4c845820b75831049d798761beafee3883794
Raheel <s8murahe@stud.uni-saarland.de>
2016-10-05 23:30:44 +0200

    teting

--- Rnbeads-Interface/application/server.R
+++ Rnbeads-Interface/application/server.R
@@ -346,8 +346,8 @@ shinyServer(function(input, output, session) {
 
     cd_list <- list()
     cd_list_counter <- 1
-    common.datasets = datasets_groups(results.dir())
-
+    #common.datasets = datasets_groups(results.dir())
+    common.datasets <- list()
 
 
     #if ( file.exists( isolate({ paste(dirfolder()[1],'index.html',sep="/") }) ) ){
